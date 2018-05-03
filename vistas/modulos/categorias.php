5f7ca4824d567cb30b251db33f7b1ff947bc2bec
StevensVera <stjaveraen@ittepic.edu.mx>
2018-05-02 19:34:22 -0600

    Maquetando Modulo de Categorias

--- vistas/modulos/categorias.php
+++ vistas/modulos/categorias.php
@@ -3,16 +3,12 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-
         Administrar Categorias
-
-        <small>Panel de Control</small>
-
       </h1>
 
       <ol class="breadcrumb">
 
-        <li><a href="#"><i class="fa fa-dashboard"></i>Inicio</a></li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i>Inicio</a></li>
 
         <li class="active">Administrar Categorias</li>
         
@@ -24,28 +20,147 @@
 
       <!-- Default box -->
       <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+        <div class="box-header with-border">
+
+          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCategoria">
+            
+            Agregar Categorias
+
+          </button>
+
         </div>
+        
+
         <div class="box-body">
-          Start creating your amazing application!
+        
+          <table class="table table-bordered table-striped dt-responsive tablas">
+              
+              <thead>
+                <tr>
+
+                  <th style="width: 10px">#</th>
+                  <th>Categoria</th>
+                  <th style="width: 5px">Acciones</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>EQUIPOS ELECTROMECÁNICOS</td>
+                    <td>
+                      <div class="btn-group">
+
+                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                        <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                        
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>EQUIPOS ELECTROMECÁNICOS</td>
+                    <td>
+                      <div class="btn-group">
+
+                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                        <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                        
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>EQUIPOS ELECTROMECÁNICOS</td>
+                    <td>
+                      <div class="btn-group">
+
+                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                        <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                        
+                      </div>
+                    </td>
+                  </tr>
+
+              </tbody>  
+          
+          </table>
+
         </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+
       </div>
-      <!-- /.box -->
 
     </section>
-    <!-- /.content -->
+   
   </div>
+ <!--*=============================================
+      =            AGREGAR USUARIOS           =
+      =============================================*/-->
+
+
+<div id="modalAgregarCategoria" class="modal fade" role="dialog">
+
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+
+      
+      <form role="form" method="post">
+      <!--*=============================================
+      =            CABEZA DEL MODAL           =
+      =============================================*/-->
+
+      <div class="modal-header" style="background: #3c8dbc; color:white">
+
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+        <h4 class="modal-title">Agregar Categoria</h4>
+
+      </div>
+
+       <!--*=============================================
+          =            CUERPO DEL MODAL          =
+      =============================================*/-->
+      <div class="modal-body">
+
+        <div class="box-body">
+
+          <!-- ENTRADA PARA EL NOMBRE -->
+          
+          <div class="form-group">
+
+            <div class="input-group">
+              
+              <span class="input-group-addon"><i class="fa fa-th"></i></span>
+
+              <input type="text" class="form-control input-lg" name="nuevaCategoria" placeholder="Ingresar Categoria" required>
+
+            </div>
+            
+          </div>
+
+        </div>
+        
+      </div>
+      <!--*=============================================
+      =            PIE DEL MODAL           =
+      =============================================*/-->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+        
+        <button type="submin" class="btn btn-primary">Guardar Categoria</button>
+
+      </div>
+
+      
+
+    </form>
+    </div>
+
+  </div>
+</div>
